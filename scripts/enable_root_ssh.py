c82385958290d37edf5a45bf2ba7c526e3bef7d3
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-02-15 23:16:50 +0100

    Remove cruft config adjustments from enable_root_ssh
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- scripts/enable_root_ssh.py
+++ scripts/enable_root_ssh.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python2
 
-with open('.config', 'a') as f:
-    f.write('CONFIG_PACKAGE_openssh-client=y\n' + \
-            'CONFIG_PACKAGE_openssh-client-utils=y\n' + \
-            'CONFIG_PACKAGE_openssh-keygen=y\n' + \
-            'CONFIG_PACKAGE_openssh-sftp-client=y\n' + \
-            'CONFIG_PACKAGE_openssh-sftp-server=y\n' + \
-            'CONFIG_PACKAGE_openssh-server=y\n' + \
-            'CONFIG_PACKAGE_openssh-server-pam=y\n')
-
 with open('openwrt/files/etc/passwd', 'r') as f:
     lines = f.readlines()
     root_ent = [i for i, line in enumerate(lines) if 'root' in line]
